ba9ed06091c64861ed2ed9041d7d4d93c8af02dd
buzz <hitmanbuzz@proton.me>
2026-02-16 13:50:14 +0530

    add tracing init for client code

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,9 +1,18 @@
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
 
 const IP_ADDR: &str = "127.0.0.1:4096";
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default subscriber failed");
+    
     let new_stream = tokio::net::TcpStream::connect(IP_ADDR).await;
     match new_stream {
         Ok(mut stream) => {
