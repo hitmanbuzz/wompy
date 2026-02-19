7bbfd069409979940440793a7ddad681e5e9703d
buzz <hitmanbuzz@proton.me>
2026-02-19 20:09:58 +0530

    separate the client send/write part to a separate rust file

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,7 +1,11 @@
 use eframe::egui;
-use tokio::{io::AsyncWriteExt, sync::mpsc};
+use tokio::sync::mpsc;
 use tracing::Level;
 use tracing_subscriber::FmtSubscriber;
+
+use crate::init::{connect_server, send_init_data};
+
+mod init;
 
 const IP_ADDR: &str = "127.0.0.1:4096";
 const GOOD_SYM: &str = "✅";
@@ -248,59 +252,4 @@ async fn main() -> eframe::Result<()> {
     )?;
 
     Ok(())
-}
-
-async fn connect_server(ip_addr: &str) -> tokio::net::TcpStream {
-    let new_stream = tokio::net::TcpStream::connect(ip_addr).await;
-
-    match new_stream {
-        Ok(stream) => return stream,
-        Err(e) => {
-            match e.kind() {
-                std::io::ErrorKind::HostUnreachable => {
-                    panic!("Host Unreachable (Not Found)");
-                },
-                std::io::ErrorKind::AddrNotAvailable => {
-                    panic!("Host not found");
-                },
-                std::io::ErrorKind::ConnectionRefused => {
-                    panic!("Got rejected by the host");
-                }
-                _ => {
-                    panic!("Got Error: {}", e);
-                }
-            }
-        },
-    }
-}
-
-/// Send username and groupname after connecting to the server
-async fn send_init_data(
-    user_name: &str,
-    group_name: &str,
-    stream: &mut tokio::net::TcpStream,
-) -> bool {
-    let username = format!("{}\n", user_name);
-    let groupname = format!("{}\n", group_name);
-
-    let (mut user_good, mut group_good) = (false, false);
-
-    if let Ok(_) = stream.write_all(username.as_bytes()).await {
-        user_good = true;
-    }
-
-    if let Ok(_) = stream.write_all(groupname.as_bytes()).await {
-        group_good = true;
-    }
-
-    if user_good && group_good {
-        tracing::info!(
-            "user `{}` has joined the server in group: `{}`",
-            username.trim(),
-            groupname.trim()
-        );
-        return true;
-    }
-
-    return false;
 }
